1ae88460ac71fe296ea7965975eb6bb1527067b0
Mohamed Machlou <medmachlou@gmail.com>
2025-08-23 09:56:21 +0100

    create and add Get All Method to  admin service

---
+++ src/app/core/services/admin-service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Admin } from '../../models/admin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+
+  http = inject(HttpClient);
+  adminUrl = 'http://localhost:3000/machlouadmin'
+
+
+    // Get All Admin
+  getAllAdmins(): Observable<Admin[]>{
+    return this.http.get<Admin[]>(`${this.adminUrl}`);
+  }
+
+}
